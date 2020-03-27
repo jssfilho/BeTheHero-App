b2dcd65a09b985a62840063cb6d1330504f1a94b
joao santos <j_ssfilho@hotmail.com>
2020-03-27 09:48:09 -0300

    BackEnd 'Finalizado'

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController.js');
+const IncidentController = require('./controllers/IncidentController.js');
+const ProfileController = require('./controllers/ProfileController.js');
+const SessionController = require('./controllers/SessionController.js')
+
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs' , OngController.index);
+routes.post('/ongs', OngController.create);
+
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
